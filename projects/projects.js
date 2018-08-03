311142b08c52e36536bb1427bad298e09caee645
nighttime <softwareatnight@gmail.com>
2018-08-03 00:22:19 -0400

    added contour map

--- projects/projects.js
+++ projects/projects.js
@@ -5,26 +5,13 @@ projects = [
         body: `I have written several projects demonstrating concepts in modern machine learning. I am fascinated by neural-inspired architectures and language processing.
         Check out my projects on <a href="https://github.com/nighttime/DL-Projects">Github</a>.`
     },
+// <iframe src="https://player.vimeo.com/video/282962530" width="640" height="640" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
     {
-        pics: ['lightning.png'],
-        title: 'Lightning Notes',
-        body: `Lightning Notes is a note-taking web app. It understands the way you take notes, styling and structuring text as you type.
-        You define styling rules, which can affect anything from a single keyword to complex patterns in your text. Lightning structures your document so it's ready to go, right away.
-        I developed the product idea for Lightning, led interface design efforts, and engineered the text comprehension engine, which dynamically compiles user rules into regexes.
-        <a href="https://vimeo.com/138032443">Check it out in action!</a>`
-    },
-    {
-        pics: ['tti.png'],
-        title: 'Two to Infinity',
-        body: `2048: Two to Infinity began with a simple mission: to redo 2048 with <em>style</em>.
-        I built this iOS game from the ground up to be fresh, charming, and engaging. It includes achievements, unlockable themes, leaderboards, and limitless play. I hope you enjoy it!
-        To date, Two to Infinity has 11k downloads. You can <a class="contact-link" href="https://www.youtube.com/watch?v=DarccjOP6zE">watch</a> the trailer and <a class="contact-link" href="https://itunes.apple.com/us/app/beyond-2048-two-to-infinity!/id860372644?mt=8">play</a> it yourself.`
-    },
-    {
-        pics: ['digital_bear.png', 'digital_wolf.png'],
-        title: 'wild things',
-        body: `I enjoy digital illustration in addition to interface design.
-        This series is an ongoing project that I add to from time to time.`
+        pics: ['contour.png'],
+        title: 'contour map',
+        video: '<iframe id="modal-video" src="https://player.vimeo.com/video/282962530?autoplay=1&loop=1&title=0&portrait=0" width height frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>',
+        body: `A topographical contour map models a region's geography by demarcating regions by elevation. I was inspired by these maps: how they form organic shapes, yet still obey a set of rules. Here I interpret abstract form through imagined motion.
+        This project was brought to life with Processing and a custom OpenGL shader. It is modeled using randomly generated bivariate Gaussian functions that roam across an imaginary map. The geography has equal height along each contour line.`
     },
     {
         pics: ['growing.png'],
@@ -34,6 +21,27 @@ projects = [
         I wanted to explore organic motion dictated entirely by computation. Every aspect of the scene is generated probabilistically, which means that each tree is unique.
         The leaves were painted and scanned by my partner. I conceived the concept and programmed the scene in Processing, carefully tuning and easing by hand to achieve motion that truly feels alive.
         Check out the source code <a class="contact-link" href="https://github.com/nighttime/growing">here</a>.`
+    },
+    {
+        pics: ['tti.png'],
+        title: 'Two to Infinity',
+        body: `2048: Two to Infinity began with a simple mission: to redo 2048 with <em>style</em>.
+        I built this iOS game from the ground up to be fresh, charming, and engaging. It includes achievements, unlockable themes, leaderboards, and limitless play. I hope you enjoy it!
+        To date, Two to Infinity has 11k downloads. You can <a class="contact-link" href="https://www.youtube.com/watch?v=DarccjOP6zE">watch</a> the trailer and <a class="contact-link" href="https://itunes.apple.com/us/app/beyond-2048-two-to-infinity!/id860372644?mt=8">play</a> it yourself.`
+    },
+    {
+        pics: ['lightning.png'],
+        title: 'Lightning Notes',
+        body: `Lightning Notes is a note-taking web app. It understands the way you take notes, styling and structuring text as you type.
+        You define styling rules, which can affect anything from a single keyword to complex patterns in your text. Lightning structures your document so it's ready to go, right away.
+        I developed the product idea for Lightning, led interface design efforts, and engineered the text comprehension engine, which dynamically compiles user rules into regexes.
+        <a href="https://vimeo.com/138032443">Check it out in action!</a>`
+    },
+    {
+        pics: ['digital_bear.png', 'digital_wolf.png'],
+        title: 'wild things',
+        body: `I enjoy digital illustration in addition to interface design.
+        This series is an ongoing project that I add to from time to time.`
     },
     {
         pics: ['echo.png'],
