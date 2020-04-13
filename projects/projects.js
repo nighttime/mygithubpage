005cfa0300947750b0a49eaee4cb0fadf8bcfc25
Nick <nmckenna363@gmail.com>
2020-04-14 00:08:15 +0100

    updated project text

--- projects/projects.js
+++ projects/projects.js
@@ -10,31 +10,32 @@ projects = [
         pics: ['contour.png'],
         title: 'contour map',
         video: '<iframe id="modal-video" src="https://player.vimeo.com/video/282962530?autoplay=1&loop=1&title=0&portrait=0" width height frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>',
-        body: `A topographical contour map models a region's geography by demarcating regions by elevation. I was inspired by these maps: how they form organic shapes, yet still obey a set of rules. Here I interpret abstract form through imagined motion.
-        This project was brought to life with Processing and a custom OpenGL shader. It is modeled using randomly generated bivariate Gaussian functions that roam across an imaginary map. The geography has equal height along each contour line. The closer the lines, the steeper the incline.`
+        body: `A topographical contour map models a region's geography by demarcating elevation. I was inspired by these maps: how they form organic shapes, yet still obey a set of rules and are thus interpretable. Here I interpret abstract form through imaginary motion.
+        This project was brought to life with Processing and a custom OpenGL shader. It is modeled using randomly generated bivariate Gaussian functions that roam across an imaginary map. The geography has equal height along each contour line. The closer the lines, the steeper the incline.
+        You can view the source code <a href="https://github.com/nighttime/Contour-Map" target="_blank">here</a>.`
     },
     {
         pics: ['growing.png'],
         title: 'growing',
         video: '<iframe id="modal-video" src="https://player.vimeo.com/video/178700892?autoplay=1&loop=1&title=0&portrait=0" width height frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>',
         body: `For this collaborative art project, I was inspired by vegetation and natural growth.
-        I wanted to explore organic motion dictated entirely by computation. Every aspect of the scene is generated probabilistically, which means that each tree is unique.
-        The leaves were painted and scanned by my partner. I conceived the concept and programmed the scene in Processing, carefully tuning and easing by hand to achieve motion that truly feels alive.
-        Check out the source code <a href="https://github.com/nighttime/growing" target="_blank">here</a>.`
+        I wanted to explore randomized, organic motion. Every aspect of the scene is generated probabilistically, similar to an L-system, which means that each tree is structured but unique.
+        The leaves were hand-painted and scanned by my partner, and the scene is programmed in Processing.
+        You can view the source code <a href="https://github.com/nighttime/growing" target="_blank">here</a>.`
     },
     {
         pics: ['tti.png'],
         title: 'Two to Infinity',
         body: `2048: Two to Infinity began with a simple mission: to redo 2048 with <em>style</em>.
-        I built this iOS game from the ground up to be fresh, charming, and engaging. It includes achievements, unlockable themes, leaderboards, and limitless play. I hope you enjoy it!
-        To date, Two to Infinity has 11k downloads. You can <a href="https://www.youtube.com/watch?v=DarccjOP6zE" target="_blank">watch</a> the trailer and <a href="https://itunes.apple.com/us/app/beyond-2048-two-to-infinity!/id860372644?mt=8" target="_blank">play</a> it yourself.`
+        This iOS game was built from the ground up to be fresh, charming, and engaging. It includes achievements, unlockable themes, leaderboards, and limitless play. I hope you enjoy it!
+        TTI was launched in 2014 and to date has over 16k downloads. You can <a href="https://www.youtube.com/watch?v=DarccjOP6zE" target="_blank">watch</a> the trailer and <a href="https://itunes.apple.com/us/app/beyond-2048-two-to-infinity!/id860372644?mt=8" target="_blank">play</a> it yourself.`
     },
     {
         pics: ['lightning.png'],
         title: 'Lightning Notes',
-        body: `Lightning Notes is a note-taking web app. It understands the way you take notes, styling and structuring text as you type.
+        body: `Lightning Notes is a note-taking web app. It understands the way you take notes and styles your writing <em>as you write it.</em>
         You define styling rules, which can affect anything from a single keyword to complex patterns in your text. Lightning structures your document so it's ready to go, right away.
-        I developed the product idea for Lightning, led interface design efforts, and engineered the text comprehension engine, which dynamically compiles user rules into regexes.
+        Lightning is powered by a text comprehension engine which dynamically compiles user-defined rules into regexes used for styling text as you type.
         <a href="https://vimeo.com/138032443" target="_blank">Check it out in action!</a>`
     },
     {
